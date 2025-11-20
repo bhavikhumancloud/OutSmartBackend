66b3fbb1764bf64cd2a93d79f850b8bde700f357
Bhavikmohod <bhavik.mohod@humancloud.co.in>
2025-11-20 14:08:48 +0530

    Committing the changes

--- BackendAPIOutsmart/src/test/java/tests/ExampleTest.java
+++ BackendAPIOutsmart/src/test/java/tests/ExampleTest.java
@@ -11,16 +11,17 @@ public class ExampleTest extends BaseTest {
 
     @Test(description = "GET /posts returns 200 and list")
     public void getPostsShouldReturnList() {
-        Response res = api.get("/posts", Map.of("userId", 1));
+        Response res = api.get("api/users", Map.of("page", 2));
         Assert.assertEquals(res.getStatusCode(), 200, "Expected 200 OK");
-        Assert.assertTrue(res.jsonPath().getList("$").size() > 0, "Expected non-empty list");
+        System.out.println(res.asString());
+//        Assert.assertTrue(res.jsonPath().getList("$").size() > 0, "Expected non-empty list");
     }
 
-    @Test(description = "POST /posts creates post")
-    public void createPostShouldReturn201() {
-        var body = Map.of("title", "foo", "body", "bar", "userId", 1);
-        Response res = api.post("/posts", body);
-        Assert.assertTrue(res.getStatusCode() == 201 || res.getStatusCode() == 200, "Expected 201/200");
-        Assert.assertEquals(res.jsonPath().getString("title"), "foo");
-    }
+//    @Test(description = "POST /posts creates post")
+//    public void createPostShouldReturn201() {
+//        var body = Map.of("title", "foo", "body", "bar", "userId", 1);
+//        Response res = api.post("/posts", body);
+//        Assert.assertTrue(res.getStatusCode() == 201 || res.getStatusCode() == 200, "Expected 201/200");
+//        Assert.assertEquals(res.jsonPath().getString("title"), "foo");
+//    }
 }
