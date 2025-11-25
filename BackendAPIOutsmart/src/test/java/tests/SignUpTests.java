5278739d8cffd1e7add6bec6ef57f61812839dc4
Bhavikmohod <bhavik.mohod@humancloud.co.in>
2025-11-25 15:04:58 +0530

    updated the signUpTest & signIn_SignOutTest file

--- BackendAPIOutsmart/src/test/java/tests/SignUpTests.java
+++ BackendAPIOutsmart/src/test/java/tests/SignUpTests.java
@@ -1,5 +1,7 @@
 package tests;
 
+import api.ApiClient;
+import api.BaseTest;
 import io.restassured.RestAssured;
 import io.restassured.response.Response;
 import org.testng.Assert;
@@ -7,36 +9,32 @@ import org.testng.annotations.Test;
 
 import java.util.Map;
 
-public class SignUpTests {
+public class SignUpTests extends BaseTest {
 
-    @Test(description = "Verify user signup returns 201 and success message")
+    @Test(description = "Sign up endpoint /be/iam/api/signup/")
     public void signUpTest() {
+        try{
+            String path = "/be/iam/api/signup/";
 
+            String uniqueEmail = "priti" + System.currentTimeMillis() + "@gmail.com";
 
+            Map<String, String> body = Map.of(
+                    "name", "Priti",
+                    "email", uniqueEmail,
+                    "phone_number", "+919526543264",
+                    "password", "Test@123"
+            );
 
-        // Dynamic email so each test run uses a fresh email
-        String uniqueEmail = "priti" + System.currentTimeMillis() + "@gmail.com";
+            Response response = api.post(path,body);
+            System.out.println("Response for sign up is: "+response.asString());
+            // Validations
+            Assert.assertEquals(response.getStatusCode(), 201, "Expected status code 201");
+            Assert.assertEquals(response.jsonPath().getString("detail"), "Successful sign up.", "Expected success message");
 
-        // Request Body
-        Map<String, String> requestBody = Map.of(
-                "name", "Priti",
-                "email", uniqueEmail,
-                "phone_number", "+919526543265",
-                "password", "Test@123"
-        );
+        }catch (Exception e){
+            e.printStackTrace();
+            Assert.fail("Exception occurred during sign up test: " + e.getMessage());
+        }
 
-        // API request
-        Response response = RestAssured
-                .given()
-                .header("Content-Type", "application/json")
-                .body(requestBody)
-                .post("/be/iam/api/signup/");
-
-        // Print Response
-        response.prettyPrint();
-
-        // Validations
-        Assert.assertEquals(response.getStatusCode(), 201, "Expected status code 201");
-        Assert.assertEquals(response.jsonPath().getString("detail"), "Successful sign up.", "Expected success message");
     }
 }
