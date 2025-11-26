10d65e06d96c48da0b016b3b7db3b5df50c6d073
Bhavikmohod <bhavik.mohod@humancloud.co.in>
2025-11-26 16:43:30 +0530

    Added community endpoints

--- BackendAPIOutsmart/src/test/java/tests/ExcelReadTest.java
+++ BackendAPIOutsmart/src/test/java/tests/ExcelReadTest.java
@@ -1,6 +1,7 @@
 package tests;
 
 import api.BaseTest;
+import api.Config;
 import org.testng.annotations.Test;
 import utils.ExcelUtil;
 
@@ -9,25 +10,30 @@ import java.io.IOException;
 public class ExcelReadTest extends BaseTest {
 
     ExcelUtil excelutil = new ExcelUtil();
+//    String excelPath =  Config.get("excelPath");
+//    String rootPath = System.getProperty("user.dir");
+    String ExcelFile = System.getProperty("user.dir") + Config.get("excelPath");
     @Test
     public void testExcelRead() throws IOException {
         // Test implementation goes here
-        excelutil.ExcelUtils("C:\\Users\\HC-LEN-THINKPAD-02\\OneDrive - Humancloud Technologies Pvt. Ltd\\24Nov\\BackendAPIOutsmart\\src\\test\\resources\\TestDataOutsmart.xlsx","SignUp");
+        excelutil.ExcelUtils(ExcelFile,"SignUp");
         int romCount = excelutil.getRowCount();
         System.out.println("Total number of rows: " + romCount);
     }
 
     @Test(description = "write the date in excel file")
     public void testExcelWrite() throws IOException {
-        // Test implementation goes here
-        excelutil.ExcelUtils("C:\\Users\\HC-LEN-THINKPAD-02\\OneDrive - Humancloud Technologies Pvt. Ltd\\24Nov\\BackendAPIOutsmart\\src\\test\\resources\\TestDataOutsmart.xlsx","SignUp");
+
+        excelutil.ExcelUtils(ExcelFile,"SignUp");
         String cellData = excelutil.getCellData(0,1);
         System.out.println("Cell data at (0,1): " + cellData);
         excelutil.setCellValue(2,0,cellData);
 
-        excelutil.writeExcelFile("C:\\Users\\HC-LEN-THINKPAD-02\\OneDrive - Humancloud Technologies Pvt. Ltd\\24Nov\\BackendAPIOutsmart\\src\\test\\resources\\TestDataOutsmart.xlsx");
+        excelutil.writeExcelFile(ExcelFile);
 
         String colData = excelutil.getCellData(2,0);
         System.out.println("Cell data at (2,0): " + colData);
+
+        excelutil.saveExcelFile();
     }
 }
